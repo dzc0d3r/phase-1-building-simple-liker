f8145c227e60ef83076154bc7396c978e4355be5
dzc0d3r <m4dcod3r@protonmail.com>
2023-08-30 23:20:23 +0100

    Completed assignements :tada:

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
@@ -23,3 +23,25 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+const likes = document.querySelectorAll(".like-glyph")
+const modal = document.getElementById("modal")
+modal.classList.add("hidden")
+
+likes.forEach(like => {
+  like.addEventListener("click", (e) => {
+    e.preventDefault()
+    mimicServerCall().then(() => {
+      like.innerHTML = FULL_HEART
+      like.style.color = "red"
+      like.style.fontSize = "20px"
+      modal.classList.add("hidden")
+
+    }).catch((error) => {
+      modal.textContent = error
+      modal.classList.remove("hidden")
+    })
+
+
+  })
+})
